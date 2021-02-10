c828bd8ba1755aa31e0702d536fee7d6591c968e
Ruby-Walkover <ruby@whozzat.com>
2021-02-10 18:26:05 +0530

    user page after login

---
+++ portfolio.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<img src="img/Ruby.jpg" alt="Ruby" class="center"> 
+    <h1>Ruby Kumari (SORUKI)</h1>
+    <p class="b">Btech from  <a href="https://www.iitk.ac.in/ ">IIT Kanpur</a> <br>
+    <img src="img/map.jpeg" alt="Location" width=30px> Jharkhand <br> Currently I am in pre-final year in Biological Science and Bioengineerign department.
+    <br>Technical-intern at <a href="https://walkover.in/">WALKOVER</a> in the product<a href="https://msg91.com/"> MSG91.</a> <br>
+    Manager of SOFTWARE CORNER at <a href="https://techkriti.org/"> TECHKRITI'21 </a> IIT Kanpur. <br>
+    Sports person:
+    (High-jump, Long-jump, Volleyball, Squash, Taekwondo) <br>
+    FANTASY: become guitarist, dot drawing artist. <br>
+    Some interesting Courses taken:
+    </p>
+    <ul class="course">
+    <li>Data structure and Algorithm</li>
+    <li>Cybersecurity and its ten domains</li>
+    <li>Introduction to Psychology</li>
+    <li>Psychology of Language</li>
+    </ul>
+    <p>Technology: </p> 
+    <p>C/C++ Python PHP HTML/CSS DJANGO mySQL Javascript</p>
+    <p class="b">Contact me on: <br>
+        <img src="img/insta.jpg_fit=scale" alt="INSTA" width=60px style="vertical-align:middle"> 
+        <a href="https://www.instagram.com/sorukiiii/">sorukiiii</a>  <br>
+        <img src="img/facebook.png" alt="facebook" width=60px style="vertical-align:middle">
+        <a href="https://www.facebook.com/rubymehta.mehta.5/">rubymehta</a> <br> 
+        <img src="img/twitter.png" alt="Twitter" width=60px style="vertical-align:middle"> 
+        <a href="https://twitter.com/RubyKum50251188">RubyKumari</a>  <br>
+        <img src="img/email.jpeg" alt="email" width=60px style="vertical-align:middle"> 
+        <a href="rubykri.kumari26@gmail.com">rubykri.kumari26@gmail.com</a>  <br>
+    </p>
+
+</body>
+</html>
